5faeaa9655e9a6238ac538caeed67e503415da54
RazzorFlame <razzorflame@o2.pl>
2017-12-04 16:35:45 +0100

    #pragma once instead of header guard.

--- AdvancedGDK/include/Misc/Pointers.hpp
+++ AdvancedGDK/include/Misc/Pointers.hpp
@@ -1,5 +1,4 @@
-#ifndef ADVANCEDGDK_POINTERS_HPP_INCLUDED
-#define ADVANCEDGDK_POINTERS_HPP_INCLUDED
+#pragma once
 
 #include <memory>
 
@@ -8,9 +7,4 @@ namespace agdk
 	template <typename T>	using s_ptr = std::shared_ptr<T>;
 	template <typename T>	using u_ptr = std::unique_ptr<T>;
 	template <typename T>	using w_ptr = std::weak_ptr<T>;
-	
-	template <typename T>	using observer	= T *;
-
 } // namespace agdk
-
-#endif //!ADVANCEDGDK_POINTERS_HPP_INCLUDED
