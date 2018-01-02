895960807f18d13efdd586dca4238d227c0a89c6
RazzorFlame <razzorflame@o2.pl>
2018-01-02 18:23:10 +0100

    Added main header file used from external projects.

---
+++ AdvancedGDK/include/AdvancedGDK.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "SampGDK.hpp"
+
+// Misc
+#include "Misc/Clock.hpp"
+#include "Misc/Color.hpp"
+#include "Misc/Exceptions.hpp"
+#include "Misc/Math.hpp"
+#include "Misc/Pointers.hpp"
+#include "Misc/Random.hpp"
+#include "Misc/String.hpp"
+#include "Misc/Task.hpp"
+
+// Player
+#include "Player/Player.hpp"
+#include "Player/PlayerPool.hpp"
+
+// Server
+#include "Server/GameMode.hpp"
+#include "Server/Server.hpp"
