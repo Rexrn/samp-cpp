9a2ff60deee48774ba14b952e5ecd11e57d9b7ed
RazzorFlame <razzorflame@o2.pl>
2018-01-02 21:42:42 +0100

    Added unit tests project.

---
+++ UnitTests/MathLengthUnitTest.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"			// PCH
+#include "CppUnitTest.h"	// Unit test library.
+// Unit test namespace:
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+// AdvancedGDK:
+#include <AdvancedGDK/AdvancedGDK.hpp>
+
+// Standard includes:
+
+
+// Unit test:
+namespace UnitTests
+{		
+	TEST_CLASS(MathLengthUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(LightYearsValueTest)
+		{
+			agdk::Lightyears fiveLightYears = 5;
+
+			// Compute how many metres 5ly is:
+			agdk::Meters metresValue = fiveLightYears;
+
+
+		}
+
+	};
+}
