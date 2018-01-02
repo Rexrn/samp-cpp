51c64c1b9c9f42985dbeb6fb72f6ba6b4137908e
RazzorFlame <razzorflame@o2.pl>
2018-01-02 18:21:54 +0100

    Better seed implementation with commentary inside Random number generator.

--- AdvancedGDK/src/Misc/Random.cpp
+++ AdvancedGDK/src/Misc/Random.cpp
@@ -1,6 +1,9 @@
-#include "stdafx.h"				// The precompiled header.
+#include "stdafx.h"				// PCH.
+
+// Custom includes:
 #include <Misc/Random.hpp>		// The `Random` header.
 
+// Standard includes:
 #include <chrono>				// for std::chrono::system_clock
 
 namespace agdk
@@ -13,7 +16,21 @@ namespace agdk
 		// Note: older platforms can crash on std::random_device. If it crashes set `cxprNoRandomDevice` to true.
 		if constexpr (cxprNoRandomDevice)
 		{
-			static std::mt19937 mtEngine{ std::chrono::system_clock::now().time_since_epoch().count() };
+			// We need to convert system_clock::time_point::duration to int32_t seconds.
+			// Simple static_cast could give incorrect values (losing precision), so we use duration_cast to int32_t seconds.
+			// std::mt19937 engine constructor requires std::uint32_t seed, so best we can do is just taking an absolute value.
+			// Steps of generating seed:
+			//   1. Take time since epoch (system_clock::time_point::duration | most likely duration<std::int64_t, std::nano>);
+			//   2. Cast it to duration<std::int32_t>;
+			//   3. Get count of seconds (allow negative values) - std::int32_t;
+			//   4. Get absolute seconds value;
+			//   5. Cast it to std::uint32_t.
+			// This method is used to prevent crashing when somebody set system time to pre-epoch (needs verification).
+
+			using int32seconds = std::chrono::duration<std::int32_t>;
+			static std::mt19937 mtEngine{
+				static_cast<std::uint32_t>( std::abs( std::chrono::duration_cast<int32seconds>(std::chrono::system_clock::now().time_since_epoch()).count() ) )
+			};
 			return mtEngine;
 		}
 		else
