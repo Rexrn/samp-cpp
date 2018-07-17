590e09f1d5765f09621dbed6d07c80e1cd8f8fd5
Paweł S <rfpawelsyska@gmail.com>
2018-07-17 06:21:49 +0200

    Disabled copying TestGM to server plugins.

--- Examples/TestGM/CopyToServerPlugins.bat
+++ Examples/TestGM/CopyToServerPlugins.bat
@@ -1,28 +1,3 @@
 @echo off
-
-:: [ Manual configuration ]
-
-:: Configure this variable to point to samp server \plugins directory.
-:: Example:
-:: Lets say, your folder has this path:
-::
-:: C:\Path\To\SAMPServer\plugins
-::
-:: The line below should look like this:
-::
-:: SET "TargetDir=C:\Path\To\SAMPServer\plugins"
-::
-:: Do not add slash or backslash at the end.
-SET "TargetDir=D:\Inne\SampServer\plugins"
-
-:: [ Automatic configuration (parameters taken from Visual Studio) ]
-SET "SourceFile=%1"
-SET "TargetName=%2"
-SET "TargetFile=%TargetDir%\%TargetName%.dll"
-
-
-:: Note: prints run command for debug purposes:
-:: echo "echo f | xcopy /f /y %SourceFile% %TargetFile%"
-
-echo Copying built binary to server \plugins directory!
-echo f | xcopy /f /y %SourceFile% %TargetFile%
+:: Note: Copying to server plugins is disabled for now. Rename "CopyToServerPlugins (enabled).bat" if you want to automatically copy it.
+echo Copying to server /plugins for TestGM is disabled. 
