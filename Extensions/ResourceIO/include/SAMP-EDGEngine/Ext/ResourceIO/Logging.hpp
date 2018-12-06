561555187d67ca44e2df53dad2bac951fda64864
PoetaKodu <poetakodupl@gmail.com>
2018-12-06 08:36:36 +0100

    Created file with logging configuration.

---
+++ Extensions/ResourceIO/include/SAMP-EDGEngine/Ext/ResourceIO/Logging.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <string_view>
+
+namespace samp_edgengine::ext::resource_io
+{
+constexpr std::string_view ModuleName			= "SAMP-EDGEngine-Ext-ResourceIO";
+constexpr std::string_view ModuleLogPrefix		= "[SAMP-EDGEngine-Ext-ResourceIO]";
+}
