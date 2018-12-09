ec715ac0381bd002e7dc59fbbc7fae4de99aea15
PoetaKodu <poetakodupl@gmail.com>
2018-12-09 18:32:47 +0100

    Removed unnecessary constness and "...Type" postfixes.

--- Engine/include/SAMP-EDGEngine/Core/BasicInterfaces/Updatable.hpp
+++ Engine/include/SAMP-EDGEngine/Core/BasicInterfaces/Updatable.hpp
@@ -10,16 +10,16 @@ namespace samp_edgengine
 /// </summary>
 struct IUpdatable
 {
-	using ClockType = std::chrono::high_resolution_clock;
-	using TimePointType = ClockType::time_point;
-	using DurationType = TimePointType::duration;
+	using Clock		= std::chrono::high_resolution_clock;
+	using TimePoint = Clock::time_point;
+	using Duration	= TimePoint::duration;
 
 	/// <summary>
 	/// Updates object every frame.
 	/// </summary>
 	/// <param name="deltaTime_">Number of seconds passed since last update.</param>
 	/// <param name="timeMoment_">The point of time in which update happened.</param>
-	virtual void update(double const deltaTime_, TimePointType const & timeMoment_) = 0;
+	virtual void update(double deltaTime_, TimePoint timeMoment_) = 0;
 };
 
 
