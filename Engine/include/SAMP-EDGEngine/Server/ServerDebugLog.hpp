10b1a1eeeb740cf98ce7c8d7bd253b6eec26c6f3
PoetaKodu <rfpawelsyska@gmail.com>
2019-01-06 14:07:56 +0100

    Fixed debug log.

--- Engine/include/SAMP-EDGEngine/Server/ServerDebugLog.hpp
+++ Engine/include/SAMP-EDGEngine/Server/ServerDebugLog.hpp
@@ -9,9 +9,9 @@
 extern class samp_edgengine::IGameMode* GameMode;
 
 #ifdef DEBUG
-	#define EDGE_LOG_DEBUG(messageType_, format_, ...) \
+	#define EDGE_LOG_DEBUG(messageType_, ...) \
 		if (GameMode) \
-			GameMode->debugLog.push(samp_edgengine::LogMessage:: messageType_, samp_edgengine::text::ascii::format(format_, __VA_ARGS__));
+			GameMode->debugLog.push( samp_edgengine::LogMessage:: messageType_, samp_edgengine::text::ascii::format( __VA_ARGS__ ) );
 #else
 	#define EDGE_LOG_DEBUG(...)
 #endif
